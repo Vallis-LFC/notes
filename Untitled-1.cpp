b413728b266b9adb5e1ef9e01d631bf7be72229e
Vallis Leung <vkh.luong@gmail.com>
2023-05-30 17:31:10 -0400

    notes

---
+++ Untitled-1.cpp
@@ -0,0 +1,25 @@
+//notes(this should be C and not C++)
+
+#include <stdio.h> //basic lib the one below too
+#include <stdlib.h>
+#include <math.h> //math lib
+
+int main(){
+    /*is this a comment*/
+    const int AGE = 5; //const makes it unmodifiable *(giving upper case is common practice)
+    // d for int, f for float, s is str, c is char
+    int age;
+    char name[20];
+    printf("enter age: ");
+    scanf("%d", &age); // & to store single char, num
+    printf("%d",age);
+
+    //fgets makes it more general, includes space, also stores enter key
+    fgets(name, 20, stdin);    //stdn makes it a standard input
+
+    scanf("%s", name);
+
+    printf("%s",name);
+
+    return 0;
+}
